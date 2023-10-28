4d8d1e6e1ca02484ce7ba8d9d7838fa18449e7e0
Kevyn Murilo <132490286+KevynMurilo@users.noreply.github.com>
2023-10-28 10:02:04 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,7 @@ import {
     ContainerText,
     Span,
     Text,
+    Button,
     ContainerAnimations,
     SocialIcons,
     ImgIconPrincipal,
@@ -30,6 +31,7 @@ import Volei from '../../assets/volei.png';
 import Peteca from '../../assets/petaca.png';
 import Frisbee from '../../assets/frisbee.png';
 import IconBase from '../../assets/IconPrincipal.jpeg';
+import Raquete from '../../assets/raquete.png';
 
 
 import ImageBottom from '../../assets/wave.svg'
@@ -57,6 +59,7 @@ export default function Home(){
             <Content>
                 <ContainerText>
                         <Text>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in <Span>laying out print</Span>, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</Text>
+                        <Button>PLAY</Button>
                 </ContainerText>
 
                 <ContainerAnimations>
@@ -66,6 +69,7 @@ export default function Home(){
                         <ImgIcons src={Volei} alt="Social3"/>
                         <ImgIcons src={Peteca} alt="Social4"/>
                         <ImgIcons src={Frisbee} alt="Social5"/>
+                        <ImgIcons src={Raquete} alt="Social5"/>
                     </SocialIcons>
                     <ImgIconPrincipal src={IconBase} alt="EmailIcon"/>
 
